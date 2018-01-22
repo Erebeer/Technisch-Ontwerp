0420ae58c1eee25423df2a722700dda6efdcfcb7
Eric Louwers <ericlouwers@outlook.com>
2018-01-22 11:45:29 +0000

    Functie generate() aangemaakt. Zorgt ervoor dat hij de API inleest en een dictionary met vraag:antwoord teruggeeft

--- helpers.py
+++ helpers.py
@@ -1,6 +1,6 @@
 import csv
 import urllib.request
-
+import json
 from flask import redirect, render_template, request, session
 from functools import wraps
 
@@ -12,3 +12,24 @@ def error(message, topmessage="ERROR"):
             s = s.replace(old, new)
         return s
     return render_template("error.html", top=topmessage, bottom=escape(message))
+
+def generate():
+    # Open the API
+    api =  "https://opentdb.com/api.php?amount=10"
+    webpage = list(urllib.request.urlopen(api))
+
+    # Generates the questionset
+    temp = json.loads(webpage[0].decode("utf-8"))['results']
+
+    # Create lists of questions and correct answers
+    all_questions = []
+    correct_answers = []
+    for x in temp:
+        all_questions.append(x['question'])
+    for x in temp:
+        correct_answers.append(x['correct_answer'])
+
+    # Combines both lists into a dict (key = question, value = correct answer)
+    questionset = dict(zip(all_questions, correct_answers))
+
+    return(questionset)
