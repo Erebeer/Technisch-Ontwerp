7a6794b11a3c69916ed4c86938eb5e00d165d49a
Lars Bremmers <bremmerslars@hotmail.com>
2018-01-23 17:10:12 +0000

    game.py ingevoegd bij application.py

--- application.py
+++ application.py
@@ -94,6 +94,31 @@ def leaderboards():
 
 @app.route("/play", methods=["GET", "POST"])
 def play():
+    questions = helpers.generate()
+
+# Set score
+    score = 0
+
+
+    for x in questions:
+        correct_answer = questions[x][1].lower()
+        print(correct_answer)
+        print(questions[x][0])
+        print("Your Answer: ")
+        answer = input()
+
+        if answer == correct_answer:
+            print("Correct!")
+            score = score + 100
+        elif answer == "pass":
+            print("passed, you'll lose 50 points")
+            score = score - 50
+        else:
+            score = score - 100
+            print("Wrong, you'll lose 100 points")
+        print("current score: ",score)
+
+    print("Total Score: ", score)
     return render_template("game.html")
 
 @app.route("/logout", methods=["GET", "POST"])
@@ -104,8 +129,6 @@ def logout():
 
     # Go back to the homepage
     return render_template("logout.html")
-
-
 
 
 if __name__ == "__main__":
